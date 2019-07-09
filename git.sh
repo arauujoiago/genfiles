0525cc875e4798d11f6f8953fedd1510ba48e783
Iago <iago@facex.com.br>
2019-07-09 17:47:18 -0300

    Alguma Coisa.

--- git.sh
+++ git.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-echo -e "arauujoiago\n" | git add . && git commit -m "Alguma Coisa." | git push 
-echo -e "iagodz1994\n"
+git add . && git commit -m "Alguma Coisa."
+git push
+echo "arauujoiago\n" 
+echo "iagodz1994\n"
